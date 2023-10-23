1ea21bf34eca43c0014fccaea7febdb975e78f37
김재웅 <jwkim@kocom.com>
2023-10-23 09:40:22 +0900

    add blur

--- app/src/main/java/com/example/allinone/FragmentActivity.java
+++ app/src/main/java/com/example/allinone/FragmentActivity.java
@@ -5,29 +5,82 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
-
-import com.example.allinone.testframgent.CctvFragment;
+import android.widget.ImageView;
+import android.widget.TimePicker;
 
 public class FragmentActivity extends AppCompatActivity {
+
     FrameLayout frameLayoutRoot=null;
+    FrameLayout frameLayoutActionSheet=null;
+
+    FrameLayout mBackScreen=null;
+
+
     Fragment cctvFragment=null;
+    Button btnAction=null;
+
+
+    Context context=null;
+
+
+    private ImageView mBackgroundBlurImageView=null;
+
+    TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker timePicker, int i, int i1) {
+            Log.d("test", "onTimeSet() i="+i+", i1="+i1);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
 
-        frameLayoutRoot = findViewById(R.id.idMainFrame);
+        context = this;
 
-        cctvFragment = new CctvFragment();
+        frameLayoutRoot = findViewById(R.id.idMainFrame);
+        btnAction = findViewById(R.id.idBtnAction);
+
+        frameLayoutActionSheet = new FrameLayout(this);
+        FrameLayout.LayoutParams pr=new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,FrameLayout.LayoutParams.MATCH_PARENT);
+        frameLayoutActionSheet.setLayoutParams(pr);
+        frameLayoutRoot.addView(frameLayoutActionSheet);
+
+        //addBlurBackGroundImage(frameLayoutRoot);
+        //cctvFragment = new CctvFragment();
+
+        //home screen clip
+        //scr.setClipChildren(false);//default root view clip
+        //scr.setClipToPadding(false);
+
+        btnAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //make base screen for app icons.
+                //LayoutInflater inflater = (LayoutInflater) context.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+                //mBackScreen=(FrameLayout) inflater.inflate(R.layout.layout_fragment_cctv, frameLayoutActionSheet, false);
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(context, 2, onTimeSetListener,11,30,true);
+                timePickerDialog.show();
+            }
+        });
     }
+
 
     @Override
     protected void onResume() {
         super.onResume();
-        startFragment();
+        //startFragment();
     }
 
     @Override
@@ -42,6 +95,32 @@ public class FragmentActivity extends AppCompatActivity {
         //replace
         //fragmentTransaction.addToBackStack(tag);
         fragmentTransaction.commit();
+    }
+
+
+
+    private void addBlurBackGroundImage(FrameLayout parentLayout)
+    {
+        killBlurBackGroundImage();
+        ViewGroup.LayoutParams lp=new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        mBackgroundBlurImageView=new ImageView(parentLayout.getContext());
+        mBackgroundBlurImageView.setLayoutParams(lp);
+        mBackgroundBlurImageView.setBackgroundColor(Color.parseColor("#77000000"));
+        parentLayout.addView(mBackgroundBlurImageView);
+    }
+
+    private void killBlurBackGroundImage()
+    {
+        //ImageView view=mBackScreen.findViewWithTag("TAG_BLUR_IMAGE");
+
+        try {
+            if(mBackgroundBlurImageView!=null) {
+                ((ViewGroup)mBackgroundBlurImageView.getParent()).removeView(mBackgroundBlurImageView);
+                mBackgroundBlurImageView=null;
+            }
+        } catch (NullPointerException e) {
+            //Log.e(TAG, "killBlurBackGroundImage() "+e);
+        }
     }
 
 }
